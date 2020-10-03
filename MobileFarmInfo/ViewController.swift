1508544619caa44722c5017a83ee04b37dd46443
PJ Gray <pj@evertrue.com>
2020-10-03 16:56:28 -0400

    Initial add of project

---
+++ MobileFarmInfo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MobileFarmInfo
+//
+//  Created by PJ Gray on 10/3/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
