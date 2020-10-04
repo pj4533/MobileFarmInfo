10d2293dfee15fc4f20808836e9cacba85bcf3db
PJ Gray <pj@evertrue.com>
2020-10-03 21:06:44 -0400

    More setup

--- MobileFarmInfo/FarmViewController.swift
+++ MobileFarmInfo/FarmViewController.swift
@@ -9,10 +9,23 @@ import UIKit
 
 class FarmViewController: UIViewController {
 
+    enum CellTypes {
+        case header, token1, token2, totalStaked, rewards, apy, staked, earnings
+    }
+    
+    let cells : [CellTypes] = [
+        .header
+    ]
+    
+    let pools : [Pool] = [
+        Pool(name: "Pair name")
+    ]
+    
+    @IBOutlet weak var tableview: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.title = "Pools"
     }
     
 
@@ -27,3 +40,30 @@ class FarmViewController: UIViewController {
     */
 
 }
+
+extension FarmViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.cells.count
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        let pool = self.pools[section]
+        return pool.name
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+//        let pool = self.pools[indexPath.section]
+        
+        switch self.cells[indexPath.row] {
+        case .header:
+            cell.textLabel?.text = "header"
+        default:
+            print("unknown")
+        }
+                
+        return cell
+    }
+}
+
---
+++ MobileFarmInfo/Models/Pool.swift
@@ -0,0 +1,12 @@
+//
+//  Pool.swift
+//  MobileFarmInfo
+//
+//  Created by PJ Gray on 10/3/20.
+//
+
+import Foundation
+
+struct Pool : Codable {
+    var name : String?
+}
